f6a1583e57a47761e3f6e54520926ed5c2cad29d
Oleg Lavrovsky <oleg@utou.ch>
2013-01-17 08:15:00 +0100

    Server migration and style updates

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
-
-/* Browser language detection */
-switch (substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)){
-    case "de":
-        include("index.de.php");
-        break;
-    case "fr":
-        include("index.fr.php");
-        break;
-    default:
-        include("index.en.php");
-        break;
-}
-
-?>
+/**
+ * Forwarder to doku.php
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Andreas Gohr <andi@splitbrain.org>
+ */
+header("Location: doku.php");
