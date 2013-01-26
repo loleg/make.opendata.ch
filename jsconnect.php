7d80b076aeb8c2522b813e3d9879716127a9196d
Oleg Lavrovsky <oleg@utou.ch>
2013-01-26 02:06:59 +0100

    Added jsConnect

---
+++ jsconnect.php
@@ -0,0 +1,53 @@
+<?php
+require_once dirname(__FILE__).'/functions.jsconnect.php';
+
+/* DokuWiki setup */
+if(!defined('DOKU_INC')) define('DOKU_INC',dirname(__FILE__).'/');
+require_once(DOKU_INC.'inc/init.php');
+
+if(!isset($_SERVER['REMOTE_USER'])) die('');
+if(!exportUserDoku()) die('hmm');
+
+// Get DokuWiki data
+function exportUserDoku() {
+	global $auth;
+	if (!$auth) return false;
+	
+	// 1. Get your client ID and secret here. These must match those in your jsConnect settings.
+	$clientID = "197411298";
+	$secret = "267158a45e323ba7e31436c9ca64fc00";
+
+	// 2. Grab the current user from your session management system or database here.
+	$signedIn = true; // this is just a placeholder	
+	
+	$user_id   = $_SERVER['REMOTE_USER'];
+		
+	$userinfo = $auth->getUserData($user_id);
+	if(!$userinfo['mail']) {
+		return false;
+	}
+	
+	$user_name = $userinfo['name'];
+	$user_mail = $userinfo['mail'];
+		
+	// 3. Fill in the user information in a way that Vanilla can understand.
+	$user = array();
+
+	if ($signedIn) {
+	   // CHANGE THESE FOUR LINES.
+	   $user['uniqueid'] = $user_id;
+	   $user['name'] = $user_id;
+	   $user['email'] = $user_mail;
+	   $user['photourl'] = '';
+	}
+
+	// 4. Generate the jsConnect string.
+
+	// This should be true unless you are testing. 
+	// You can also use a hash name like md5, sha1 etc which must be the name as the connection settings in Vanilla.
+	$secure = true; 
+	WriteJsConnect($user, $_GET, $clientID, $secret, $secure);
+	
+	
+	return true;
+}
